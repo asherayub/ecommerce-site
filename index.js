1d71b99d39a51b93f09c466219d32ccfe21f21d3
Asher Ayub <99917058+asherayub@users.noreply.github.com>
2022-08-16 01:22:05 +0100

    final-change

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const cart = document.querySelector(".cart")
+cart.addEventListener("click", () => {
+    document.querySelector(".bought-item").classList.toggle("d-block")
+})
 
+const addToCart = document.querySelectorAll(".product-buy-btn")
+addToCart.forEach((buy) => {
+    buy.addEventListener("click", (e) => {
+        let addItemToCart = document.querySelector(".bought-item")
+        addItemToCart.appendChild(e.target.parentNode.parentNode.parentNode)
+    })
+})
